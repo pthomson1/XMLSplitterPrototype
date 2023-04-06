1147213525800cdb730a8c076ce01055a3a16384
Paul Thomson <Pthomson@wgeld.org>
2023-04-06 08:51:18 -0400

    Add project files.

---
+++ XMLSplitter/XMLSplitter.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace XMLSplitter
+{
+    partial class XMLSplitter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.openXMLDialog = new System.Windows.Forms.OpenFileDialog();
+            this.splitButton = new System.Windows.Forms.Button();
+            this.filepathlbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(105, 189);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(204, 68);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Select XML File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // openXMLDialog
+            // 
+            this.openXMLDialog.FileName = "openFileDialog1";
+            // 
+            // splitButton
+            // 
+            this.splitButton.Location = new System.Drawing.Point(409, 189);
+            this.splitButton.Name = "splitButton";
+            this.splitButton.Size = new System.Drawing.Size(204, 68);
+            this.splitButton.TabIndex = 1;
+            this.splitButton.Text = "Split File";
+            this.splitButton.UseVisualStyleBackColor = true;
+            this.splitButton.Click += new System.EventHandler(this.splitButton_Click);
+            // 
+            // filepathlbl
+            // 
+            this.filepathlbl.AutoSize = true;
+            this.filepathlbl.Location = new System.Drawing.Point(132, 86);
+            this.filepathlbl.Name = "filepathlbl";
+            this.filepathlbl.Size = new System.Drawing.Size(0, 13);
+            this.filepathlbl.TabIndex = 2;
+            this.filepathlbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // XMLSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 396);
+            this.Controls.Add(this.filepathlbl);
+            this.Controls.Add(this.splitButton);
+            this.Controls.Add(this.openButton);
+            this.Name = "XMLSplitter";
+            this.Text = "XMLSplitter";
+            this.Load += new System.EventHandler(this.XMLSplitter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.OpenFileDialog openXMLDialog;
+        private System.Windows.Forms.Button splitButton;
+        private System.Windows.Forms.Label filepathlbl;
+    }
+}
+
---
+++ XMLSplitter/XMLSplitter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace XMLSplitter
+{
+
+    public partial class XMLSplitter : Form
+    {
+        public XMLSplitter()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            if (openXMLDialog.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.xmlFilePath = openXMLDialog.FileName;
+                Properties.Settings.Default.Save();
+                filepathlbl.Text = ($"File Loaded : {Properties.Settings.Default.xmlFilePath} ");
+            }
+        }
+
+
+        private void Split()
+        {
+            
+            int index = 0;
+            int size = 700;
+            XDocument xdoc = XDocument.Load(Properties.Settings.Default.xmlFilePath);
+            xdoc.Descendants("MetersNotRead").Remove();
+            xdoc.Descendants("MetersRead").Remove();
+            xdoc.Descendants("ScheduleExecution").Remove();
+
+
+            foreach (var batch in xdoc.Root.Elements().InSetsOf(size))
+            {
+                var newDoc = new XDocument(
+                     new XElement("AMRDEF", batch));
+
+                newDoc.Save($"C:\\Users\\pthomson\\Desktop\\test_{++index}.xml");
+            }
+        }
+
+
+        private void splitButton_Click(object sender, EventArgs e)
+        {
+            Split();
+
+        }
+
+
+
+        private void XMLSplitter_Load(object sender, EventArgs e)
+        {
+            filepathlbl.Text = ($"File loaded : {Properties.Settings.Default.xmlFilePath}");
+        }
+    }
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<List<T>> InSetsOf<T>(this IEnumerable<T> source, int max)
+        {
+            List<T> toReturn = new List<T>(max);
+            foreach (var item in source)
+            {
+                toReturn.Add(item);
+                if (toReturn.Count == max)
+                {
+                    yield return toReturn;
+                    toReturn = new List<T>(max);
+                }
+            }
+            if (toReturn.Any())
+            {
+                yield return toReturn;
+            }
+        }
+    }
+}
